567d97f3ee1491b523f12799ee93b7336d8e7954
Thomas Marks <tmsluigi@gmail.com>
2022-12-03 15:24:02 -0700

    Adding the thread logic

--- cmake/Sanitizers_Working.cmake
+++ cmake/Sanitizers_Working.cmake
@@ -38,3 +38,17 @@ if(${CMAKE_BUILD_TYPE} STREQUAL "MemorySanitizer")
   set_tests_properties(memory_sanitizer_basic_test
                        PROPERTIES WILL_FAIL TRUE LABELS "Sanitizer_Working")
 endif(${CMAKE_BUILD_TYPE} STREQUAL "MemorySanitizer")
+
+# Ensure the sanitizers are catching simple test
+if(${CMAKE_BUILD_TYPE} STREQUAL "ThreadSanitizer")
+  message(STATUS "Adding thread sanitizer tests for ${CMAKE_PROJECT_NAME}")
+  # Add one executable target for the unit tests
+  add_executable(thread_sanitizer_basic_test
+                 ../test/test_sanitizers_working/test_thread_sanitizer.cpp)
+  # Add a test for ctest
+  add_test(NAME "thread_sanitizer_basic_test"
+           COMMAND thread_sanitizer_basic_test)
+  # This should fail if the memory sanitizer is working correctly
+  set_tests_properties(thread_sanitizer_basic_test
+                       PROPERTIES WILL_FAIL TRUE LABELS "Sanitizer_Working")
+endif(${CMAKE_BUILD_TYPE} STREQUAL "ThreadSanitizer")
