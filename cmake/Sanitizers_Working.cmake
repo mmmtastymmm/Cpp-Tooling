8859fc07bf9dbe32114a74538eb201d540a8328a
Thomas Marks <tmsluigi@gmail.com>
2022-12-03 14:23:43 -0700

    Clean up the cmake

--- cmake/Sanitizers_Working.cmake
+++ cmake/Sanitizers_Working.cmake
@@ -1,16 +1,11 @@
 # Ensure the sanitizers are catching simple test
-message(${CMAKE_BUILD_TYPE})
 if(${CMAKE_BUILD_TYPE} STREQUAL "MemorySanitizer")
-  message(STATUS "Did enable memory sanitizer tests for ${CMAKE_PROJECT_NAME}")
-  # Will enable tests now
-  enable_testing()
+  message(STATUS "Adding memory sanitizer tests for ${CMAKE_PROJECT_NAME}")
   # Add one executable target for the unit tests
-  add_executable(memory_sanitizer_fail_test
+  add_executable(memory_sanitizer_basic_test
                  ../test/test_sanitizers_working/test_memory_sanitizer.cpp)
-  # Link with the catch2 main file
-  target_link_libraries(memory_sanitizer_fail_test PRIVATE Catch2)
   # Add a test for ctest
-  add_test(NAME "memory_sanitizer_fail_test" COMMAND memory_sanitizer_fail_test)
-  # Add test that will fail
-  set_tests_properties(memory_sanitizer_fail_test PROPERTIES WILL_FAIL TRUE)
+  add_test(NAME "memory_sanitizer_basic_test" COMMAND memory_sanitizer_basic_test)
+  # This should fail if the memory sanitizer is working correctly
+  set_tests_properties(memory_sanitizer_basic_test PROPERTIES WILL_FAIL TRUE LABELS "Sanitizer_Working")
 endif(${CMAKE_BUILD_TYPE} STREQUAL "MemorySanitizer")
