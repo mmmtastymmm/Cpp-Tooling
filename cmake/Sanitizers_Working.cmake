724f980092d938892d660a214632600dc4ac2941
Thomas Marks <tmsluigi@gmail.com>
2022-12-03 14:24:17 -0700

    Format

--- cmake/Sanitizers_Working.cmake
+++ cmake/Sanitizers_Working.cmake
@@ -5,7 +5,9 @@ if(${CMAKE_BUILD_TYPE} STREQUAL "MemorySanitizer")
   add_executable(memory_sanitizer_basic_test
                  ../test/test_sanitizers_working/test_memory_sanitizer.cpp)
   # Add a test for ctest
-  add_test(NAME "memory_sanitizer_basic_test" COMMAND memory_sanitizer_basic_test)
+  add_test(NAME "memory_sanitizer_basic_test"
+           COMMAND memory_sanitizer_basic_test)
   # This should fail if the memory sanitizer is working correctly
-  set_tests_properties(memory_sanitizer_basic_test PROPERTIES WILL_FAIL TRUE LABELS "Sanitizer_Working")
+  set_tests_properties(memory_sanitizer_basic_test
+                       PROPERTIES WILL_FAIL TRUE LABELS "Sanitizer_Working")
 endif(${CMAKE_BUILD_TYPE} STREQUAL "MemorySanitizer")
