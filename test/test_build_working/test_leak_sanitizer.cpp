66f68355e86f852812264957703fe4f85762b9a5
Thomas Marks <tmsluigi@gmail.com>
2023-01-01 00:19:39 -0700

    New format

--- test/test_build_working/test_leak_sanitizer.cpp
+++ test/test_build_working/test_leak_sanitizer.cpp
@@ -1,9 +1,9 @@
 /// Leaks memory that the leak sanitizer should catch
 /// \return 0, leak some memory as well
 #include <cstdlib>
-[[maybe_unused]] void *p;
+[[maybe_unused]] void* p;
 int main() {
     p = malloc(7);
-    p = nullptr;// The memory is leaked here.
+    p = nullptr; // The memory is leaked here.
     return 0;
 }
--- test/test_build_working/test_memory_sanitizer.cpp
+++ test/test_build_working/test_memory_sanitizer.cpp
@@ -2,8 +2,8 @@
 
 /// Jump off of uninitialized memory
 /// \return 0, side effect of printing out a message if the uninitialized memory is read
-int main(int argc, char **) {
-    int *a = new int[10];
+int main(int argc, char**) {
+    int* a = new int[10];
     // Jump off uninitialized memory
     if (not a[argc])
         printf("The uninitialized memory at index %d was zero", argc);
