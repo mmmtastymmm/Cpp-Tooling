57348f3ae604fb59419c9648102e96f1d0bec8e0
Thomas Marks <tmsluigi@gmail.com>
2023-01-01 00:12:43 -0700

    change clang-format

--- test/unit_tests.cpp
+++ test/unit_tests.cpp
@@ -3,11 +3,11 @@
 struct ContainerHolds12 {
     int* x;
 
-    ContainerHolds12(){
-        x = new int (12);
+    ContainerHolds12() {
+        x = new int(12);
     }
 
-    ~ContainerHolds12(){
+    ~ContainerHolds12() {
         delete x;
     }
 };
@@ -20,7 +20,7 @@ TEST_CASE("Example_2", "[t2]") {
     CHECK(4.0 == std::pow(2, 2));
 }
 
-TEST_CASE("Example_Heap", "[t3]"){
+TEST_CASE("Example_Heap", "[t3]") {
     const auto container = ContainerHolds12();
     CHECK(*container.x == 12);
 }
