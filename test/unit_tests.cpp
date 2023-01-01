aa584cfb04311cfd256c6ba0871d059e6b101403
Thomas Marks <tmsluigi@gmail.com>
2023-01-01 00:44:07 -0700

    Update some margines

--- test/unit_tests.cpp
+++ test/unit_tests.cpp
@@ -1,4 +1,5 @@
 #include <catch2/catch_all.hpp>
+#include <catch2/catch_test_macros.hpp>
 
 struct ContainerHolds12 {
     int* x;
@@ -12,15 +13,15 @@ struct ContainerHolds12 {
     }
 };
 
-TEST_CASE("Example_Test", "[t1]") {
+TEST_CASE("Example_Int", "[example_tests]") {
     CHECK(1 == 2 - 1);
 }
 
-TEST_CASE("Example_2", "[t2]") {
-    CHECK(4.0 == std::pow(2, 2));
+TEST_CASE("Example_Float", "[example_tests]") {
+    Catch::Matchers::WithinAbs(4.0 == std::pow(2, 2), 1e-19);
 }
 
-TEST_CASE("Example_Heap", "[t3]") {
+TEST_CASE("Example_Heap", "[example_tests]") {
     const auto container = ContainerHolds12();
     CHECK(*container.x == 12);
 }
